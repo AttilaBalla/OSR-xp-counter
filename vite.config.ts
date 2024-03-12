66bfba913df8aa92d7628bd19dcd33dcaa552772
Attila Balla <attila.balla@lhsystems.com>
2024-03-12 09:55:14 +0100

    add base url to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://attilaballa.github.io/OSR-xp-counter/'
 })
